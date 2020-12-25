612866a2d18af1c1df442f18d0ff8dd793fb01e9
Logan Won-Ki Lee <logan.wonki.lee@gmail.com>
2020-12-25 21:11:34 +1100

    implemented multiple riddles and fixed infinite loop while implementing reseting letters map using useEffect.

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -5,9 +5,11 @@ import Letters from '../Letters/Letters';
 let nth = 1;
 
 const Game = props => {
-	// jump to next one if solved?
-	if (props.state.solved) {
+	// If player solves cuurent riddle and it is not the last one
+	if (props.state.solved && !props.state.finished) {
+		// then play the next riddle and try to finish
 		props.next(++nth);
+		props.finish(nth);
 	}
 
 	return (
@@ -15,6 +17,7 @@ const Game = props => {
 			<Progress solution={props.state.solution} correctLetters={props.state.correct_letters} />
 			<Letters isSolved={props.state.solved} solved={props.solved} solution={props.state.solution} correct={props.correct} wrong={props.wrong} />
 			<div>{JSON.stringify(props.state)}</div>
+			<div>{props.state.solved && props.state.finished ? 'finished' : 'still riddles remain'}</div>
 		</div>
 	);
 };
--- src/Letters/Letters.js
+++ src/Letters/Letters.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AvailableLetter from './AvailableLetter';
 import DisabledLetter from './DisabledLetter';
 
@@ -8,6 +8,17 @@ const Letters = props => {
             "a":false,"b":false,"c":false,"d":false,"e":false,"f":false,"g":false,"h":false,"i":false,"j":false,"k":false,"l":false,"m":false,"n":false,"o":false,"p":false,"q":false,"r":false,"s":false,"t":false,"u":false,"v":false,"w":false,"x":false,"y":false,"z":false
         }
     );
+
+    // reset letters map only after riddle is just solved and not every time.
+    useEffect(()=>{
+        if (props.isSolved) {
+            setLettersMap(
+                {
+                    "a":false,"b":false,"c":false,"d":false,"e":false,"f":false,"g":false,"h":false,"i":false,"j":false,"k":false,"l":false,"m":false,"n":false,"o":false,"p":false,"q":false,"r":false,"s":false,"t":false,"u":false,"v":false,"w":false,"x":false,"y":false,"z":false
+                }
+            );
+        }        
+    }, [props.isSolved]);
 
     const updateClickedHandler = (letter) => {
         setLettersMap(
