863ad9187b7da187c345b70dcafa07e1e91b94c2
Logan Won-Ki Lee <logan.wonki.lee@gmail.com>
2021-01-04 16:26:24 +1100

    added 20 solutions and hints

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,7 +1,19 @@
-const bank = ['my name is logan', 'i like coding', 'merrry xmas'];
+const bank = ['depth',  'forgetting',   'example',  'educate',
+'entire',   'horizon',  'freight',  'earliest',
+'threat',   'relieve',  'muscle',   'meant',
+'exchange',     'securing',     'obedient',     'unsanitary',
+'leisure',  'crypt',    'supervisor',   'persuade',
+];
+
+const hints = ['the distance from the top or surface to the bottom of something.',  'fail to remember.',    'a thing characteristic of its kind or illustrating a general rule.',   'give intellectual, moral, and social instruction to (someone), typically at a school or university.',
+'with no part left out; whole.',    'the line at which the earth\'s surface and the sky appear to meet.',    'goods transported in bulk by truck, train, ship, or aircraft.',    'happening or done before the usual or expected time.',
+'a statement of an intention to inflict pain, injury, damage, or other hostile action on someone in retribution for something done or not done.',   'cause (pain, distress, or difficulty) to become less severe or serious.',  'physical power; strength.',    'intend to convey or refer to (a particular thing); signify.',
+'an act of giving one thing and receiving another (especially of the same kind) in return.',    'fix or attach (something) firmly so that it cannot be moved or lost.',     'complying or willing to comply with an order or request; submissive to another\'s authority.',     'not sanitary.',
+'time when one is not working or occupied; free time.',     'an underground room or vault beneath a church, used as a chapel or burial place.',     'a person who supervises a person or an activity.',     'induce (someone) to do something through reasoning or argument.'];
 
 const game = {
-	solution: 'my name is logan',
+	solution: 'depth',
+    hint: 'the distance from the top or surface to the bottom of something.',
 	available_letters: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
 	correct_letters: [],
 	lives: 12,
@@ -70,9 +82,10 @@ export default function reducer (state = { ...game }, action ) {
 
     if (action.type === 'PLAY_NEXT') {
         const next_solution = bank[action.nth-1];
+        const next_hint = hints[action.nth-1];
 
         return {
-            ...game, solution: next_solution
+            ...game, solution: next_solution, hint: next_hint
         };
     }
 
