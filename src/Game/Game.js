78211bf8dbf82e14de45df148bc769fd6a4c0706
Logan Won-Ki Lee <logan.wonki.lee@gmail.com>
2021-01-10 11:21:58 +1100

    added RESTART? dialog with screen overlay

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Progress from '../Progress/Progress';
 import Letters from '../Letters/Letters';
 import Hangman from '../Hangman/Hangman';
+import styles from './Game.module.css';
 
 let nth = 1;
 
@@ -13,15 +14,27 @@ const Game = props => {
 		props.finish(nth);
 	}
 
+	let modal = null;
+	if (props.state.lives === 0)
+	{
+		modal = <React.Fragment>
+					<div className={styles.overlay}></div>
+					<div className={styles.askforrestart + ' ' + styles.shake}>RESTART?</div>
+				</React.Fragment>;
+	}
+
 	return (
-		<div>
-			<Hangman lives={props.state.lives} />
-			<Progress solution={props.state.solution} correctLetters={props.state.correct_letters} />
-			<Letters isSolved={props.state.solved} solved={props.solved} solution={props.state.solution} correct={props.correct} wrong={props.wrong} />
-			<div>Hint: {props.state.hint}</div>
-			<div>{JSON.stringify(props.state)}</div>
-			<div>{props.state.solved && props.state.finished ? 'finished' : 'still riddles remain'}</div>
-		</div>
+		<React.Fragment>
+			{modal}
+			<div>
+				<Hangman lives={props.state.lives} />
+				<Progress solution={props.state.solution} correctLetters={props.state.correct_letters} />
+				<Letters isSolved={props.state.solved} solved={props.solved} solution={props.state.solution} correct={props.correct} wrong={props.wrong} />
+				<div>Hint: {props.state.hint}</div>
+				<div>{JSON.stringify(props.state)}</div>
+				<div>{props.state.solved && props.state.finished ? 'finished' : 'still riddles remain'}</div>
+			</div>
+		</React.Fragment>
 	);
 };
 
